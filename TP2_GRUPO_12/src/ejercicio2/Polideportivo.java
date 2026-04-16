cb58f24a71aaed9139871010b77c809daa107ff1
Martin_Forgaral <forgaralmartinnahuel@gmail.com>
2026-04-16 04:18:29 -0300

    Agregué el método toString() modificado

--- TP2_GRUPO_12/src/ejercicio2/Polideportivo.java
+++ TP2_GRUPO_12/src/ejercicio2/Polideportivo.java
@@ -4,7 +4,7 @@ public class Polideportivo extends Edificio implements IInstalacionDeportiva {
 
 	private String nombre;
 	private int TipoDeInstalacion;
-	
+
 	public Polideportivo() {
 		
 	}
@@ -14,7 +14,7 @@ public class Polideportivo extends Edificio implements IInstalacionDeportiva {
 		this.nombre= nombre;
 		this.TipoDeInstalacion=TipoDeInstalacion;
 	}
-	
+
 	public void setTipoInstalacion(int tipoInstalacion) {
 	    this.TipoDeInstalacion = tipoInstalacion;
 	}
@@ -31,4 +31,10 @@ public class Polideportivo extends Edificio implements IInstalacionDeportiva {
 		this.nombre = nombre;
 	}
 
+
+	public String toString() {
+		return  nombre + ", tipo de instalacion " + TipoDeInstalacion + ", superficie " + getSuperficieEdificio();
+	}
+
+	
 }
